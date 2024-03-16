042751d507f36ef3852e6e74ee71db86c6976ddf
alsiesta <as@daslizenzatelier.de>
2024-03-16 22:11:36 +0100

    updated env locally and assets images

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import logging
 import uuid
 from dotenv import load_dotenv
 
+load_dotenv()
+
 from quart import (
     Blueprint,
     Quart,
@@ -28,10 +30,10 @@ bp = Blueprint("routes", __name__, static_folder="static", template_folder="stat
 MINIMUM_SUPPORTED_AZURE_OPENAI_PREVIEW_API_VERSION="2024-02-15-preview"
 
 # UI configuration (optional)
-UI_TITLE = os.environ.get("UI_TITLE") or "Contoso"
+UI_TITLE = os.environ.get("UI_TITLE") or "No Title Provided"
 UI_LOGO = os.environ.get("UI_LOGO")
 UI_CHAT_LOGO = os.environ.get("UI_CHAT_LOGO")
-UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "Start chatting"
+UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "HL missing"
 UI_CHAT_DESCRIPTION = os.environ.get("UI_CHAT_DESCRIPTION") or "This chatbot is configured to answer your questions"
 UI_FAVICON = os.environ.get("UI_FAVICON") or "/favicon.ico"
 UI_SHOW_SHARE_BUTTON = os.environ.get("UI_SHOW_SHARE_BUTTON", "true").lower() == "true"
@@ -54,8 +56,6 @@ async def favicon():
 @bp.route("/assets/<path:path>")
 async def assets(path):
     return await send_from_directory("static/assets", path)
-
-load_dotenv()
 
 # Debug settings
 DEBUG = os.environ.get("DEBUG", "false")
